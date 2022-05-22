9aa18c89870c2b83991cd58e39f4581c16ea18f7
David Yarzebinski <dyarzebinski97@gmail.com>
2022-05-22 17:08:01 -0400

    mockImplementation

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,11 +1,21 @@
-import { add, subtract } from "./sum";
+import * as sum from "./sum";
 
 describe("sum", () => {
-  describe("add", () => {
-    test('should add numbers together', () => {
-      expect(add(1, 2)).toBe(3)
-    })
+  afterEach(() => {
+    jest.restoreAllMocks();
   });
 
-  describe("subtract", () => {});
+  test("spying on and mocking implementation of module", () => {
+    // mock the implementation to be deterministic
+    jest.spyOn(sum, "add").mockImplementation((num1, num2) => num2);
+    expect(sum.add(2, 2)).toBe(2);
+  });
+
+  test("spying on and mocking implementation ONCE", () => {
+    jest.spyOn(sum, "add").mockImplementationOnce((num1, num2) => num2);
+    // this first call uses the mock above
+    expect(sum.add(2, 2)).toBe(2);
+    // this second call uses the actual code
+    expect(sum.add(2, 2)).toBe(4);
+  });
 });
